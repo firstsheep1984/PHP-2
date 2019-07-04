9536fb4cf18c93cda3b621442db6c6eb933ed051
yangyangma <yangyangma2016@gmail.com>
2019-07-04 11:56:29 -0400

    last day

--- slimfirst/index.php
+++ slimfirst/index.php
@@ -1,5 +1,8 @@
 <?php
 
+session_cache_limiter(false);
+session_start();
+
 require_once 'vendor/autoload.php';
 
 DB::$user = 'slimfirst17';
@@ -26,9 +29,39 @@ $app->get('/hello/:name', function ($name) {
 });
 
 $app->get('/hello/:name/:age', function ($name, $age) use ($app) {
-    DB::insert('people', array('name' => $name, 'age' => $age));
+    DB::insert('peopleZZZZ', array('name' => $name, 'age' => $age));
     $app->render('hello.html.twig', array('nameQQQ' => $name, 'ageQQQ' => $age));
     // echo "<p>Hello, $name, you are $age y/o</p>\n";
+});
+
+// STATE 1: first show
+$app->get('/people/add', function() use ($app) {
+    $app->render('people_add.html.twig');
+});
+
+$app->post('/people/add', function() use ($app) {
+    $name = $app->request()->post('name');
+    $age = $app->request()->post('age');
+    //
+    $errorList = array();
+    if (strlen($name) < 2 || strlen($name) > 100) {
+        array_push($errorList, "Name must be 2-100 characters long");
+        $name = "";
+    }
+    if ($age == '' || $age < 0 || $age > 150) {
+        array_push($errorList, "Age must be within 0-150 range");
+        $age = "";
+    }
+    //
+    if ($errorList) { // STATE 2: failed submission
+        $app->render('people_add.html.twig', array(
+            'errorList' => $errorList,
+            'v' => array('name' => $name, 'age' => $age)
+            ));
+    } else { // STATE 3: successful submission
+        DB::insert('people', array('name' => $name, 'age' => $age));
+        $app->render('people_add_success.html.twig');
+    }
 });
 
 $app->run();
